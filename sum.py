92f197d8f82be484e24834a655fbe016f187299c
Jayaramstar <130962284+Jayaramstar@users.noreply.github.com>
2023-04-18 12:56:05 +0530

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,15 @@
+x=int(input("Enter value of x:")
+y=int(input("Enter value of y:")
+print("sum= ",x+y)
+
+
+#greatest of 3 numbers
+x=int(input("Enter value of x:")
+y=int(input("Enter value of y:")
+z=int(input("Enter value of z:")
+if(a>b and a>c)
+print(a)
+elif(b>a and b>c)
+print(b)
+else
+print(c)
